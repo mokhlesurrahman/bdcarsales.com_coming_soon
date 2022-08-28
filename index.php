3eed033a10b465b4ee65ad713d2167af1c02ec1c
Maruf Rahman <maruf.mokhlesh@gmail.com>
2022-08-28 16:19:47 +0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="bn">
+    <head>
+        <title>bdcarsales.com - Under Construction</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+
+ <link rel="shortcut icon" href="../images/favicon.ico">
+        <!-- BEGIN: stylesheets !-->
+        <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,700&amp;subset=latin' rel='stylesheet' type='text/css'>
+        <link href="css/jquery.countdown.css" rel="stylesheet" type="text/css">
+		<link href="css/screen.css" rel="stylesheet" type="text/css">
+        <link href="css/app.css" rel="stylesheet" type="text/css">
+		<!-- END stylesheets !-->
+        
+        <!--[if lt IE 9]>
+			<script src="js/html5shiv.js"></script>
+		<![endif]-->
+    </head>
+    <body>
+
+        <!-- BEGIN: header !-->
+        <header>
+            <div id="clouds"></div>
+            
+            <div id="city"></div>
+            <div id="city2"></div>
+            
+            <div class="center">
+               <h1>​We are coming soon...</h1>
+                
+                <!-- BEGIN: counter !-->
+                <div id="counter">
+                	<div id="note"></div>
+                    <div id="countdown"></div>
+                    
+                    <p id="labels">
+                        <span>days</span>
+                        <span>hours</span>
+                        <span>min</span>
+                        <span>sec</span>
+                    </p>
+                </div>
+                <!-- END counter !-->
+                
+                <div id="social">
+                    <ul>
+                        <li><a href="#" id="fb"></a></li>
+                        <li><a href="#" id="google"></a></li>
+                        <li><a href="#" id="twitter"></a></li>
+                    </ul>
+                </div>
+            </div>
+            
+        </header>
+        <!-- END: header !-->
+
+        <!-- BEGIN: header !-->
+        <section id="content">
+            <div class="center">
+                <div id="house"></div>
+                <div id="car"></div>
+                
+                <!-- BEGIN: Information !-->
+                <!--<div id="info">
+                    <h3>Information</h3>
+                    <div class="col">
+                        <p></p>
+                    </div>
+                    <div class="col">
+                        <p></p>
+                    </div>
+                </div>-->
+                <!-- END Information !-->
+                
+                <!-- BEGIN: newsletter !-->
+                <!--<div id="newsletter">
+                    <h3>Newsletter</h3>
+                    
+                    <div id="box-input">
+                        <form action="#" method="post" id="form-newsletter">
+                            <input type="text" id="email" name="newsletter" class="required email" placeholder="Enter your email"/>
+                            <input type="submit" value="Add"/>
+                            <div class="clear"></div>
+                        </form>
+                    </div>
+                </div>-->
+                <!-- END newsletter !-->
+                
+            </div>
+        </section>
+        <!-- END header !-->
+
+        <!-- BEGIN: footer !-->
+        <footer>
+            <div class="center">
+                <p class="left">Copyright &copy; <?php echo date('Y');?> <a href="">bdcarsales.com</a></p>
+                <p class="right">Powered by <a href="http://www.powergroupbd.com/" target="_blank"><strong>Power Group BD Ltd</strong></a></p>
+            </div>
+        </footer>
+        <!-- END: footer !-->
+
+        
+        <!-- BEGIN: Scripts on bottom for perfomance !-->
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
+        <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.min.js" type="text/javascript"></script>
+        <script src="js/jquery.countdown.js" type="text/javascript"></script>
+		<script src="js/app.js" type="text/javascript"></script>
+        <!-- END Scripts on bottom for perfomance !-->
+        
+    </body>
+</html>
